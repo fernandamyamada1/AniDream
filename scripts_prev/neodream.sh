a603b7fe76583aa97195559837cb36bbf5527b52
Fernanda Miyuki Yamada <fernandamyamada1@gmail.com>
2025-10-02 15:50:38 +0900

    Delete scripts_prev directory

--- scripts_prev/neodream.sh
+++
@@ -1,5 +0,0 @@
-prefix = ""
-bash scripts/neodream_train.sh "$prefix goku" 0.0
-bash scripts/neodream_train.sh "$prefix goku" 0.3
-bash scripts/neodream_train.sh "$prefix goku" 0.5
-bash scripts/neodream_train.sh "$prefix goku" 1.0
--- scripts_prev/simple_test.sh
+++
@@ -1,7 +0,0 @@
-prefix = "flat"
-
-bash scripts/simple.sh "$prefix an anime plant-based human"
-bash scripts/simple.sh "$prefix an anime wilted plant-based human"
-bash scripts/simple.sh "$prefix an anime autumn plant-based human"
-bash scripts/simple.sh "$prefix an anime plant-based human with blooming buds"
-
